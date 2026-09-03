9d714aca12532be48d7f4aad3c9b64944202100c
nanazhii75-dev <nanazhii75@gmail.com>
2026-09-03 18:09:17 +0800

    Wire Mpv tab to launch is.xyz.mpv.MainActivity (reuse mpv-android file browser)

--- app/src/main/java/com/termux/app/MainActivity.java
+++ app/src/main/java/com/termux/app/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bottom_nav_host);
 
         contentFrame = findViewById(R.id.main_content_frame);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
