5adc6884fc0f6f88cf6876c0454beb125a58b5af
nanazhii75-dev <nanazhii75@gmail.com>
2026-09-06 14:03:53 +0800

    debug: toast onCreate/onNewIntent untuk diagnosa bug multi-tab

--- app/src/main/java/com/termux/app/activities/EditorActivity.java
+++ app/src/main/java/com/termux/app/activities/EditorActivity.java
@@ -45,12 +45,14 @@ public class EditorActivity extends AppCompatActivity {
         filePathView = findViewById(R.id.editor_file_path);
         contentInput = findViewById(R.id.editor_content_input);
 
+        Toast.makeText(this, "DEBUG onCreate, tabs=" + tabs.size(), Toast.LENGTH_LONG).show();
         handleIncomingIntent(getIntent());
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        Toast.makeText(this, "DEBUG onNewIntent, tabs sebelum=" + tabs.size() + " path=" + intent.getStringExtra(EXTRA_FILE_PATH), Toast.LENGTH_LONG).show();
         setIntent(intent);
         handleIncomingIntent(intent);
     }
